3764c24064a342151bb6732eb6a7a3f83171fb20
Alex <alex.asesan@educa.jcyl.es>
2024-11-21 13:09:27 +0100

    Indice+css y pagina login

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-    if(isset($_REQUEST['login'])){
-       header('location:login.php');
+    if(isset($_REQUEST['volver'])){
+       header('location:indexLoginLogoffTema5.php');
        exit;
     }
 ?>
@@ -12,19 +12,19 @@
         <meta name="application-name" content="indice">
         <meta name="description" content="Indice tema 3">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-        <link rel="stylesheet" href="webroot/css/indexLoginLogoffTema5.css">       
+        <link rel="stylesheet" href="webroot/css/indexProyectoTema5.css">
         <title>Alex Asensio Sanchez</title>
      </head>
      <body>
          <header>
-             <h1>Aplicacion Login logoff Tema 5</h1>
-             <form method='post'>
-                <input type="submit" name='login' id='login' value='Login'>
-             </form>
-         </header>  
-         <main>
-             
-         </main>
+            <h1>Login</h1>             
+         </header>                                      
+        
+        <form method='post'>
+            <label for='login'>Volver</label>
+            <input type="submit" name='volver' id='volver' value='Volver'>
+        </form>
+
         <footer>
             <p><a href="../index.html">Alex Asensio Sanchez</a></p>
             <p><a href="../204DWESProyectoDWES/indexProyectoDWES.php">DWES</a></p>
