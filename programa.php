214618159ae5252492747dff9154f019b112561f
Alex <alex.asesan@educa.jcyl.es>
2024-11-27 09:33:50 +0100

    Mejora a mensaje bienvenida y cambios en el campo descripcion de la tabla usuarios en la BBDD

--- programa.php
+++ programa.php
@@ -48,13 +48,13 @@
                 
                 if($oUsuarioEnCurso->T01_NumConexiones>0){                   
                     ?>
-                    <p>Bienvenido <?php echo($oUsuarioEnCurso->T01_CodUsuario) ?>, se ha conectado un total de <?php echo $oUsuarioEnCurso->T01_NumConexiones+1 ?> veces,
-                    la ultima conexion fue el dia: <?php echo date_format(new DateTime($oUsuarioEnCurso->T01_FechaHoraUltimaConexion), "d/m/Y H:i:s") ?></p>
+                    <p>Bienvenido <?php echo($oUsuarioEnCurso->T01_DescUsuario) ?>, esta es la  <?php echo $oUsuarioEnCurso->T01_NumConexiones+1 ?> vez que te conectas;
+                    usted se conectó por ultima vez el: <?php echo date_format(new DateTime($oUsuarioEnCurso->T01_FechaHoraUltimaConexion), "d/m/Y H:i:s") ?></p>
                     <?php
                 }
                 else{
                     ?>
-                        <p>Bienvenido <?php echo($oUsuarioEnCurso->T01_CodUsuario) ?>, es la primera vez que te conectas</p>
+                        <p>Bienvenido <?php echo($oUsuarioEnCurso->T01_DescUsuario) ?>, es la primera vez que te conectas</p>
                     <?php
                 }
             ?>
--- scriptDB/CargaInicialDB204DWESProyectoTema5.sql
+++ scriptDB/CargaInicialDB204DWESProyectoTema5.sql
@@ -6,14 +6,14 @@ insert into T02_Departamento values('PES', 'Pescaderia', now(), 100.34, null),
 ('LIM', 'Limpieza', now(), 3000.6, null),
 ('VER', 'Verduleria', '2024-07-19 12:00:00', 12000.56, '2024-11-02 14:00:00');
 
-insert into T01_Usuario values('admin', SHA2('adminpaso', 256), 'usuario admin', 0, null, 'perfil', null),
-('alex', SHA2('alexpaso', 256), 'usuario alex', 0, null, 'perfil', null),
-('victor', SHA2('victorpaso', 256), 'usuario victor', 0, null, 'perfil', null),
-('luis', SHA2('luispaso', 256), 'usuario luis', 0, null, 'perfil', null),
-('jesus', SHA2('jesuspaso', 256), 'usuario jesus', 0, null, 'perfil', null),
+insert into T01_Usuario values('admin', SHA2('adminpaso', 256), 'Admin Admin Admin', 0, null, 'perfil', null),
+('alex', SHA2('alexpaso', 256), 'Alex Asensio Sanchez', 0, null, 'perfil', null),
+('victor', SHA2('victorpaso', 256), 'Victor Garcia Gordon', 0, null, 'perfil', null),
+('luis', SHA2('luispaso', 256), 'Luis Ferreras Gonzalez', 0, null, 'perfil', null),
+('jesus', SHA2('jesuspaso', 256), 'Jesus Ferreras Gonzalez', 0, null, 'perfil', null),
 
-('antonio', SHA2('antoniopaso', 256), 'usuario antonio', 0, null, 'perfil', null),
-('ivan', SHA2('ivanpaso', 256), 'usuario ivan', 0, null, 'perfil', null),
-('heraclio', SHA2('heracliopaso', 256), 'usuario heraclio', 0, null, 'perfil', null),
-('amor', SHA2('amorpaso', 256), 'usuario amor', 0, null, 'perfil', null),
-('gisela', SHA2('giselapaso', 256), 'usuario gisela', 0, null, 'perfil', null);
+('antonio', SHA2('antoniopaso', 256), 'Antonio Jañez Veleda', 0, null, 'perfil', null),
+('ivan', SHA2('ivanpaso', 256), 'Ivan Campos de Cela', 0, null, 'perfil', null),
+('heraclio', SHA2('heracliopaso', 256), 'Heraclio Borbujo Moran', 0, null, 'perfil', null),
+('amor', SHA2('amorpaso', 256), 'Amor Rodriguez Navarro', 0, null, 'perfil', null),
+('gisela', SHA2('giselapaso', 256), 'Gisela Folgueral Lindoso', 0, null, 'perfil', null);
